97b78f678e2cee2d40bd2090243f551868360a23
PC9-IFLAB <soulreaper2407@gmail.com>
2023-03-21 17:07:07 +0700

    menambahkan class SimpleDataBase

--- modul5_1302210123/Program.cs
+++ modul5_1302210123/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class SimpleDataBase
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase(List<T> storedData, List<DateTime> inputDates)
+    {
+        this.storedData = storedData;
+        this.inputDates = inputDates;
+    }
+
+    public T AddNewData()
+    {
+
+    }
+
+    public void PrintAllData()
+    {
+
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+    }
+}
